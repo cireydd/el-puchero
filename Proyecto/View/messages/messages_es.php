63f2ea355415da6d7d2f401e4dc43e7da18d2bfd
Santiago <santiyitnas@gmail.com>
2021-10-31 22:34:05 +0100

    Cambios i18n

--- Proyecto/View/messages/messages_es.php
+++ Proyecto/View/messages/messages_es.php
@@ -4,6 +4,16 @@ $i18n_messages =
 array(
 	
 	"Spanish" => "Español",
+	"Contents" => "Contenido",
+	"Author" => "Autor",
+	"Actions" => "Acciones",
+	"Login" => "Entrar",
+	"Username" => "Nombre de usuario",
+	"Password" => "Contraseña",
+	"Register here!" => "¡Regístrate aquí!",
+	"Register" => "Registro",
+	"Edit" => "Editar",
+	"Delete" => "Eliminar",
 	
 )
 ?>
